643178a73a79a9460a4e4079dbc225436ee87a05
FangWeii <vodoanphuongvy@gmail.com>
2022-09-16 22:48:10 +0700

    sườn

--- script.js
+++ script.js
@@ -3,6 +3,18 @@
 
 
 // start VY
+var api="https://jsonplaceholder.typicode.com/posts"
+fetch(api)
+    .then((Response) => {
+        return Response.json()
+    })
+    .then((data) => {
+        document.getElementById("penguins-upper-content").innerHTML = (data[0].body + data[1].body);
+        document.getElementById("penguins-lower-content").innerHTML = (data[2].body + data[3].body);
+    })
+    .catch(() => {
+        console.log('lỗi api');
+    })
 // end VY
 
 
