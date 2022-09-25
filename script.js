61d5a1f619c99c93101b70bd6bd94277f22d56b8
FangWeii <vodoanphuongvy@gmail.com>
2022-09-25 07:28:48 +0700

    changes

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 
 // start VY
+// content
 var api="https://jsonplaceholder.typicode.com/posts"
 fetch(api)
     .then((Response) => {
@@ -11,10 +12,13 @@ fetch(api)
     .then((data) => {
         document.getElementById("penguins-upper-content").innerHTML = (data[0].body + data[1].body);
         document.getElementById("penguins-lower-content").innerHTML = (data[2].body + data[3].body);
+        document.getElementById("whales-upper-content").innerHTML = (data[4].body + data[5].body);
     })
     .catch(() => {
         console.log('lỗi api');
     })
+// timer
+// email
 // end VY
 
 
