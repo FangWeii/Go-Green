29fe2f4a81c822e87a239401f9498959813fbe6d
Nguyễn Đăng Anh Đức <83125962+justakidz1402@users.noreply.github.com>
2022-09-26 17:55:03 +0700

    .

--- script.js
+++ script.js
@@ -58,33 +58,33 @@ function getCategoriesKidz(x) {
     console.log(x);
 }
 
-let check = new Boolean(true);
-
-function getCategories2Kidz(x) {
-  if(check != false){
-    document.getElementById("OurMissionKidz").classList.remove("activeKidz");
-    document.getElementById("OurVisionKidz").classList.remove("activeKidz");
-    document.getElementById("OurHistoryKidz").classList.remove("activeKidz");
-    document.getElementById(x).classList.add("activeKidz");
-    for (var i = 0; i < 3; i++) {
-      document.getElementsByClassName("SubMissionKidz")[i].style.display = "none";
-    }
-    for (var i = 0; i < document.getElementsByClassName(x).length; i++) {
-      document.getElementsByClassName(x)[i].style.display = "block";
-    }
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-    document.getElementById("OurMissionKidz").classList.remove("activeKidz");
-    document.getElementById("OurVisionKidz").classList.remove("activeKidz");
-    document.getElementById("OurHistoryKidz").classList.remove("activeKidz");
-    for (var i = 0; i < 3; i++) {
-      document.getElementsByClassName("SubMissionKidz")[i].style.display = "none";
-    }
-    for (var i = 0; i < document.getElementsByClassName(x).length; i++) {
-      document.getElementsByClassName(x)[i].style.display = "none";
-    }
+    x.style.display = "none";
   }
-  
 }
+
+function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  var x = document.getElementById("myDIV3   ");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 // end DUC
 
 
