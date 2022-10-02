8b004f5b46d1d26fc8721a52e275699dca17f362
Nguyễn Đăng Anh Đức <83125962+justakidz1402@users.noreply.github.com>
2022-10-02 09:37:18 +0700

    nghia

--- script.js
+++ script.js
@@ -125,4 +125,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // start NGHIA
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".game-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
 // end NGHIA
